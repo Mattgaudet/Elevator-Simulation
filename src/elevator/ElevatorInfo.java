9557ae7bc0a37746069ffc34056efc9c9aaf369c
AliAbdollahian <ALIABDOLLAHIAN@cmail.carleton.ca>
2024-03-06 18:35:09 -0500

    Add files via upload

---
+++ src/elevator/ElevatorInfo.java
@@ -0,0 +1,33 @@
+package elevator;
+
+import floor.ElevatorRequest;
+
+public class ElevatorInfo {
+    private int elevatorId;
+    private Elevator.State currentState;
+    private int currentFloor;
+    private ElevatorRequest.ButtonDirection currDirection;
+
+    public ElevatorInfo(int elevatorId, Elevator.State currentState, int currentFloor, ElevatorRequest.ButtonDirection currDirection) {
+        this.elevatorId = elevatorId;
+        this.currentState = currentState;
+        this.currentFloor = currentFloor;
+        this.currDirection = currDirection;
+    }
+
+    public int getElevatorId() {
+        return elevatorId;
+    }
+
+    public Elevator.State getCurrentState() {
+        return currentState;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public ElevatorRequest.ButtonDirection getCurrDirection() {
+        return currDirection;
+    }
+}
